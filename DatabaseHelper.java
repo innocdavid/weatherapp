38be38c8b3227bb267a2712c6510caab9ef8c6e1
ibrahimabk <47291466+ibrahimabk@users.noreply.github.com>
2019-03-30 13:26:53 +0300

    Update DatabaseHelper.java

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -9,12 +9,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "registers.db";
-    public static final String TABLE_NAME = "Register.db";
+    public static final String TABLE_NAME = "Register";
     public static final String COL_1 = "FirstName";
     public static final String COL_2 = "Username";
     public static final String COL_3 = "Email";
     public static final String COL_4 = "Password";
-    //public static  final String COL_5="register.db";
+    
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -32,11 +32,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
-    public long addUseer(String user, String password) {
+    public long addUseer(String user, String password.,String fname, String mail) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValue = new ContentValues();
         contentValue.put("Username", user);
         contentValue.put("Password", password);
+        contentValue.put("FirstName", fname);
+        contentValue.put("Email", mail);
         Long rest = db.insert("register", null, contentValue);
         return rest;
     }
