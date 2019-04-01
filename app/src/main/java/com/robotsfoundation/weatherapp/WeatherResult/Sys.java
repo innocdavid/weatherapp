641966029b0e40afc9f9b6c486c49dc195e486ee
innocdavid <innocdavid@gmail.com>
2019-04-01 12:55:35 +0000

    Setting libraries, getting location of device and building retrofit requests to openWeatherMap

---
+++ app/src/main/java/com/robotsfoundation/weatherapp/WeatherResult/Sys.java
@@ -0,0 +1,47 @@
+package com.robotsfoundation.weatherapp.WeatherResult;
+
+public class Sys {
+
+    private double message;
+    private String country;
+    private int sunrise;
+    private int sunset;
+
+
+    //Constructor......
+    public Sys(){
+
+    }
+
+    public double getMessage() {
+        return message;
+    }
+
+    public void setMessage(double message) {
+        this.message = message;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(int sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public int getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(int sunset) {
+        this.sunset = sunset;
+    }
+}
---
+++ app/src/main/java/com/robotsfoundation/weatherapp/weather/Weather.java
@@ -0,0 +1,6 @@
+package com.robotsfoundation.weatherapp.weather;
+
+public class Weather {
+
+    public static final String APP_ID = " c418491452923bfe74a802b16f3ea876";
+}
