a483d883f3aa115f4ad8ec1ed5653c3448554d5b
JustinMinozoy <minozoyjustin@gmail.com>
2019-04-02 15:43:31 +0300

    Adding UI to MainActivity

--- app/src/main/java/com/robotsfoundation/weatherapp/weather/Weather.java
+++ app/src/main/java/com/robotsfoundation/weatherapp/weather/Weather.java
@@ -1,6 +1,9 @@
 package com.robotsfoundation.weatherapp.weather;
 
+import android.location.Location;
+
 public class Weather {
 
     public static final String APP_ID = " c418491452923bfe74a802b16f3ea876";
+    public static Location current_location = null;
 }
